d938785ae98d9106b11e020c4156653c110018e7
czf <502629556@qq.com>
2019-08-19 23:45:10 +0800

    init -- 2

--- app/src/main/java/com/czf/myvpnservice/MainActivity.java
+++ app/src/main/java/com/czf/myvpnservice/MainActivity.java
@@ -1,13 +1,44 @@
 package com.czf.myvpnservice;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+  private final int START_VPN_REQUEST_CODE = 1;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    findViewById(R.id.start_vpn).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent intent = MyVpnService.prepare(MainActivity.this);
+        if (intent != null) {
+          startActivityForResult(intent, 1);
+        } else {
+          startVpnService();
+        }
+      }
+    });
+  }
+
+  private void startVpnService() {
+
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    if (requestCode == START_VPN_REQUEST_CODE) {
+      if (resultCode == RESULT_OK) {
+        startVpnService();
+      }
+    }
   }
 }
---
+++ app/src/main/java/com/czf/myvpnservice/MyVpnService.java
@@ -0,0 +1,15 @@
+package com.czf.myvpnservice;
+
+import android.net.VpnService;
+
+public class MyVpnService extends VpnService {
+
+  public MyVpnService() {
+
+  }
+
+  @Override
+  public void onRevoke() {
+    super.onRevoke();
+  }
+}
