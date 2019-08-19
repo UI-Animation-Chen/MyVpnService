01e4f97b523d76a5fdb662365bcd7b66b6de4092
czf <502629556@qq.com>
2019-08-20 07:48:33 +0800

    add service life cycle method

--- app/src/main/java/com/czf/myvpnservice/MyVpnService.java
+++ app/src/main/java/com/czf/myvpnservice/MyVpnService.java
@@ -1,11 +1,27 @@
 package com.czf.myvpnservice;
 
+import android.content.Intent;
 import android.net.VpnService;
 
 public class MyVpnService extends VpnService {
 
   public MyVpnService() {
 
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+  }
+
+  @Override
+  public int onStartCommand(Intent intent, int flags, int startId) {
+    return super.onStartCommand(intent, flags, startId);
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
   }
 
   @Override
