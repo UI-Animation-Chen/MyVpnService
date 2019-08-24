e3b06ba692ecbe789d907abc538d3184cbb24cee
czf <502629556@qq.com>
2019-08-24 17:20:22 +0800

    add VpnServer.c

---
+++ app/src/main/java/VpnServer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+static int get_tunnel() {
+	int upd_socket_id = socket(AF_INET4, SOCK_DGRAM, );
+}
+
+static int get_raw_sock() {
+	
+}
+
+int main(int argc, char *argv[]) {
+	
+}
+
